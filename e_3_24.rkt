e1f9bf68cfbe14a3b6a8ec35aa0aee2c2b5fa3a8
n3104 <nt3104@gmail.com>
2017-10-01 21:08:26 +0900

    問題 3.24

---
+++ e_3_24.rkt
@@ -0,0 +1,49 @@
+#lang planet neil/sicp
+
+(define (make-table same-key?)
+  (let ((local-table (list '*table*)))
+
+    (define (assoc key records)
+      (cond ((null? records) false)
+            ((same-key? key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+    dispatch))
+
+
+(define (same-key-equal? key1 key2)
+  (equal? key1 key2))
+(define operation-table (make-table same-key-equal?))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
+(put 'a 'x 1)
+(get 'a 'x)
+(get 'b 'x)
+(get 'a 'y)
