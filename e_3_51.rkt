af444fd9b4d63c0a39a54c81d80f86911f28584b
n3104 <nt3104@gmail.com>
2018-03-31 20:56:58 +0900

    問題 3.51

---
+++ e_3_51.rkt
@@ -0,0 +1,49 @@
+#lang planet neil/sicp
+
+(define (stream-car stream) (car stream))
+
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (display x)
+  (newline))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define (show x)
+  (display-line x)
+  x)
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+; 以下、動作確認
+(define x (stream-map show (stream-enumerate-interval 0 10)))
+(display-line '-)
+(stream-ref x 5)
+(display-line '-)
+(stream-ref x 7)
