a4c461254b5424dac9ae53729bf48103d5702d3b
n3104 <nt3104@gmail.com>
2018-04-14 20:32:22 +0900

    問題 3.53

---
+++ e_3_53.rkt
@@ -0,0 +1,29 @@
+#lang planet neil/sicp
+
+(define (stream-car stream) (car stream))
+
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+; 以下、動作確認
+(define s (cons-stream 1 (add-streams s s)))
+
+(stream-ref s 0)
+(stream-ref s 1)
+(stream-ref s 2)
+(stream-ref s 3)
