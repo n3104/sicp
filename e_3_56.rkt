364547c2c31fa48582e4da384e8e1f798d295574
n3104 <nt3104@gmail.com>
2018-04-15 21:23:28 +0900

    問題 3.56

---
+++ e_3_56.rkt
@@ -0,0 +1,59 @@
+#lang planet neil/sicp
+(#%require (only racket/base for))
+
+(define (stream-car stream) (car stream))
+
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define (display-line x)
+  (display x)
+  (newline))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< s1car s2car)
+                  (cons-stream s1car (merge (stream-cdr s1) s2)))
+                 ((> s1car s2car)
+                  (cons-stream s2car (merge s1 (stream-cdr s2))))
+                 (else
+                  (cons-stream s1car
+                               (merge (stream-cdr s1)
+                                      (stream-cdr s2)))))))))
+
+; 以下、追加実装
+(define S (cons-stream
+           1
+           (merge
+            (scale-stream integers 2)
+            (merge
+             (scale-stream integers 3)
+             (scale-stream integers 5)))))
+
+; 以下、動作確認
+(for ([i 20])
+  (display-line (stream-ref S i)))
