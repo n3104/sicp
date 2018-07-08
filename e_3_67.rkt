b2a904b2ac911991e1b3a7f5065afb317c6f4408
n3104 <nt3104@gmail.com>
2018-07-08 21:51:42 +0900

    問題 3.67

---
+++ e_3_67.rkt
@@ -0,0 +1,53 @@
+#lang planet neil/sicp
+(#%require (only racket/base for))
+
+(define (stream-car stream) (car stream))
+
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (display-line x)
+  (display x)
+  (newline))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+; 以下、追加実装
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (interleave
+     (stream-map (lambda (x) (list (stream-car s) x))
+                 (stream-cdr t))
+     (stream-map (lambda (x) (list x (stream-car t)))
+                 (stream-cdr s)))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+; 以下、動作確認
+(for ([i 20])
+  (display-line (stream-ref (pairs integers integers) i)))
