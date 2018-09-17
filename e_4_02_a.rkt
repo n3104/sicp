c8ad85dbadb69ad3076cc727e67a75c4aba9eea5
n3104 <nt3104@gmail.com>
2018-09-17 21:14:17 +0900

    各関数の記述順を本文の順序どおりにした。

--- e_4_02_a.rkt
+++ e_4_02_a.rkt
@@ -1,42 +1,170 @@
 #lang planet neil/sicp
 
+; https://sicp.iijlab.net/fulltext/x411.html
+; 組合せ
+; eval は問題に関係する箇所なので「追加実装」に移動した。
+
+; apply
+; apply のままだとRacket本体の apply と競合するため名前を変更した。
+(define (apply-eval-procedure procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+          (procedure-body procedure)
+          (extend-environment
+           (procedure-parameters procedure)
+           arguments
+           (procedure-environment procedure))))
+        (else
+         (error
+          "Unknown procedure type -- APPLY" procedure))))
+
+; 手続きの引数
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
+
+; 条件式
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+; 並び
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (eval (first-exp exps) env))
+        (else (eval (first-exp exps) env)
+              (eval-sequence (rest-exps exps) env))))
+
+; 代入と定義
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (eval (assignment-value exp) env)
+                       env)
+  'eval-assignment)
+
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+    (eval (definition-value exp) env)
+    env)
+  'eval-definition)
+
+
 ; https://sicp.iijlab.net/fulltext/x412.html
+
+; 自己評価式
 (define (self-evaluating? exp)
   (cond ((number? exp) true)
         ((string? exp) true)
         (else false)))
 
+; 変数
 (define (variable? exp) (symbol? exp))
+
+; クォート式
+(define (quoted? exp)
+  (tagged-list? exp 'quote))
+
+(define (text-of-quotation exp) (cadr exp))
 
 (define (tagged-list? exp tag)
   (if (pair? exp)
       (eq? (car exp) tag)
       false))
 
-(define (quoted? exp)
-  (tagged-list? exp 'quote))
-
-(define (text-of-quotation exp) (cadr exp))
-
+; 代入
 (define (assignment? exp)
   (tagged-list? exp 'set!))
 
+(define (assignment-variable exp) (cadr exp))
+
+(define (assignment-value exp) (caddr exp))
+
+; 定義
 (define (definition? exp)
   (tagged-list? exp 'define))
 
-(define (if? exp) (tagged-list? exp 'if))
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
 
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp)   ; 仮パラメタ
+                   (cddr exp)))) ; 本体
+
+; lambda式
 (define (lambda? exp) (tagged-list? exp 'lambda))
 
 (define (lambda-parameters exp) (cadr exp))
 
 (define (lambda-body exp) (cddr exp))
 
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+; 条件式
+(define (if? exp) (tagged-list? exp 'if))
+
+(define (if-predicate exp) (cadr exp))
+
+(define (if-consequent exp) (caddr exp))
+
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+; begin
 (define (begin? exp) (tagged-list? exp 'begin))
 
 (define (begin-actions exp) (cdr exp))
 
+(define (last-exp? seq) (null? (cdr seq)))
+
+(define (first-exp seq) (car seq))
+
+(define (rest-exps seq) (cdr seq))
+
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+        ((last-exp? seq) (first-exp seq))
+        (else (make-begin seq))))
+
+(define (make-begin seq) (cons 'begin seq))
+
+; 手続き作用
+(define (application? exp) (pair? exp))
+
+(define (operator exp) (car exp))
+
+(define (operands exp) (cdr exp))
+
+(define (no-operands? ops) (null? ops))
+
+(define (first-operand ops) (car ops))
+
+(define (rest-operands ops) (cdr ops))
+
+; 導出された式
 (define (cond? exp) (tagged-list? exp 'cond))
+
+(define (cond-clauses exp) (cdr exp))
+
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+
+(define (cond-predicate clause) (car clause))
+
+(define (cond-actions clause) (cdr clause))
 
 (define (cond->if exp)
   (expand-clauses (cond-clauses exp)))
@@ -55,78 +183,53 @@
                      (sequence->exp (cond-actions first))
                      (expand-clauses rest))))))
 
-(define (cond-clauses exp) (cdr exp))
-
-(define (cond-else-clause? clause)
-  (eq? (cond-predicate clause) 'else))
-
-(define (cond-predicate clause) (car clause))
-
-(define (sequence->exp seq)
-  (cond ((null? seq) seq)
-        ((last-exp? seq) (first-exp seq))
-        (else (make-begin seq))))
-
-(define (last-exp? seq) (null? (cdr seq)))
-
-(define (first-exp seq) (car seq))
-
-(define (make-begin seq) (cons 'begin seq))
-
-(define (cond-actions clause) (cdr clause))
-
-(define (make-if predicate consequent alternative)
-  (list 'if predicate consequent alternative))
-
-(define (application? exp) (pair? exp))
-
-(define (operator exp) (car exp))
-
-(define (operands exp) (cdr exp))
-
-(define (assignment-variable exp) (cadr exp))
-
-(define (assignment-value exp) (caddr exp))
-
-(define (definition-variable exp)
-  (if (symbol? (cadr exp))
-      (cadr exp)
-      (caadr exp)))
-
-(define (definition-value exp)
-  (if (symbol? (cadr exp))
-      (caddr exp)
-      (make-lambda (cdadr exp)   ; 仮パラメタ
-                   (cddr exp)))) ; 本体
-
-(define (make-lambda parameters body)
-  (cons 'lambda (cons parameters body)))
-
-(define (if-predicate exp) (cadr exp))
-
-(define (if-consequent exp) (caddr exp))
-
-(define (if-alternative exp)
-  (if (not (null? (cdddr exp)))
-      (cadddr exp)
-      'false))
-
-(define (rest-exps seq) (cdr seq))
-
-(define (first-operand ops) (car ops))
-
-(define (rest-operands ops) (cdr ops))
 
 ; https://sicp.iijlab.net/fulltext/x413.html
+
+; 述語のテスト
+(define (true? x)
+  (not (eq? x false)))
+
+(define (false? x)
+  (eq? x false))
+
+; 手続きの表現
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+
+(define (procedure-parameters p) (cadr p))
+
+(define (procedure-body p) (caddr p))
+
+(define (procedure-environment p) (cadddr p))
+
+; 環境に対する操作
 (define (enclosing-environment env) (cdr env))
+
+(define (first-frame env) (car env))
 
 (define the-empty-environment '())
 
-(define (first-frame env) (car env))
+(define (make-frame variables values)
+  (cons variables values))
 
 (define (frame-variables frame) (car frame))
 
 (define (frame-values frame) (cdr frame))
+
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons var (car frame)))
+  (set-cdr! frame (cons val (cdr frame))))
+
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
 
 (define (lookup-variable-value var env)
   (define (env-loop env)
@@ -142,9 +245,6 @@
           (scan (frame-variables frame)
                 (frame-values frame)))))
   (env-loop env))
-
-(define (make-procedure parameters body env)
-  (list 'procedure parameters body env))
 
 (define (set-variable-value! var val env)
   (define (env-loop env)
@@ -172,61 +272,30 @@
     (scan (frame-variables frame)
           (frame-values frame))))
 
-(define (add-binding-to-frame! var val frame)
-  (set-car! frame (cons var (car frame)))
-  (set-cdr! frame (cons val (cdr frame))))
-
-(define (true? x)
-  (not (eq? x false)))
-
-(define (make-frame variables values)
-  (cons variables values))
-
-(define (extend-environment vars vals base-env)
-  (if (= (length vars) (length vals))
-      (cons (make-frame vars vals) base-env)
-      (if (< (length vars) (length vals))
-          (error "Too many arguments supplied" vars vals)
-          (error "Too few arguments supplied" vars vals))))
-
-; https://sicp.iijlab.net/fulltext/x411.html
-(define (no-operands? ops) (null? ops))
-
-(define (eval-assignment exp env)
-  (set-variable-value! (assignment-variable exp)
-                       (eval (assignment-value exp) env)
-                       env)
-  'ok)
-
-(define (eval-definition exp env)
-  (define-variable! (definition-variable exp)
-    (eval (definition-value exp) env)
-    env)
-  'ok)
-
-(define (eval-if exp env)
-  (if (true? (eval (if-predicate exp) env))
-      (eval (if-consequent exp) env)
-      (eval (if-alternative exp) env)))
-
-(define (eval-sequence exps env)
-  (cond ((last-exp? exps) (eval (first-exp exps) env))
-        (else (eval (first-exp exps) env)
-              (eval-sequence (rest-exps exps) env))))
-
-(define (list-of-values exps env)
-  (if (no-operands? exps)
-      '()
-      (cons (eval (first-operand exps) env)
-            (list-of-values (rest-operands exps) env))))
 
 ; https://sicp.iijlab.net/fulltext/x414.html
+(define (setup-environment)
+  (let ((initial-env
+         (extend-environment (primitive-procedure-names)
+                             (primitive-procedure-objects)
+                             the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+
+(define (primitive-implementation proc) (cadr proc))
+
 (define primitive-procedures
   (list (list 'car car)
         (list 'cdr cdr)
         (list 'cons cons)
         (list 'null? null?)
         (list '+ +)
+        (list '- -)
+        (list 'eq? eq?)
         ;⟨基本手続きが続く⟩
         ))
 
@@ -238,15 +307,7 @@
   (map (lambda (proc) (list 'primitive (cadr proc)))
        primitive-procedures))
 
-(define (setup-environment)
-  (let ((initial-env
-         (extend-environment (primitive-procedure-names)
-                             (primitive-procedure-objects)
-                             the-empty-environment)))
-    (define-variable! 'true true initial-env)
-    (define-variable! 'false false initial-env)
-    initial-env))
-
+; setup-environment 内部で primitive-procedure-names と primitive-procedure-objects を利用するため実行タイミングをここにした。
 (define the-global-environment (setup-environment))
 
 (define (apply-primitive-procedure proc args)
@@ -255,7 +316,6 @@
 
 (define apply-in-underlying-scheme apply)
 
-(define (primitive-implementation proc) (cadr proc))
 
 ; 以下、追加実装
 (define (eval exp env)
@@ -264,7 +324,7 @@
         ((quoted? exp) (text-of-quotation exp))
         ; 手続き作用の節が代入の節の前に現れる
         ((application? exp)
-         (apply-primitive-procedure (eval (operator exp) env)
+         (apply-eval-procedure (eval (operator exp) env)
                 (list-of-values (operands exp) env)))
         ((assignment? exp) (eval-assignment exp env))
         ((definition? exp) (eval-definition exp env))
