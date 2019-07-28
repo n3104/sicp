f3e6382b15ef6dbcf7a42b6f7796c4fc4fbdb897
n3104 <nt3104@gmail.com>
2019-07-29 00:07:31 +0900

    問題 4.42

---
+++ e_4_42.rkt
@@ -0,0 +1,607 @@
+#lang planet neil/sicp
+
+; https://sicp.iijlab.net/fulltext/x412.html
+
+; 自己評価式
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+        ((string? exp) true)
+        (else false)))
+
+; 変数
+(define (variable? exp) (symbol? exp))
+
+; クォート式
+(define (quoted? exp)
+  (tagged-list? exp 'quote))
+
+(define (text-of-quotation exp) (cadr exp))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+; 代入
+(define (assignment? exp)
+  (tagged-list? exp 'set!))
+
+(define (assignment-variable exp) (cadr exp))
+
+(define (assignment-value exp) (caddr exp))
+
+; 定義
+(define (definition? exp)
+  (tagged-list? exp 'define))
+
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp)   ; 仮パラメタ
+                   (cddr exp)))) ; 本体
+
+; lambda式
+(define (lambda? exp) (tagged-list? exp 'lambda))
+
+(define (lambda-parameters exp) (cadr exp))
+
+(define (lambda-body exp) (cddr exp))
+
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+; 条件式
+(define (if? exp) (tagged-list? exp 'if))
+
+(define (if-predicate exp) (cadr exp))
+
+(define (if-consequent exp) (caddr exp))
+
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+; begin
+(define (begin? exp) (tagged-list? exp 'begin))
+
+(define (begin-actions exp) (cdr exp))
+
+(define (last-exp? seq) (null? (cdr seq)))
+
+(define (first-exp seq) (car seq))
+
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+        ((last-exp? seq) (first-exp seq))
+        (else (make-begin seq))))
+
+(define (make-begin seq) (cons 'begin seq))
+
+; 手続き作用
+(define (application? exp) (pair? exp))
+
+(define (operator exp) (car exp))
+
+(define (operands exp) (cdr exp))
+
+; 導出された式
+(define (cond? exp) (tagged-list? exp 'cond))
+
+(define (cond-clauses exp) (cdr exp))
+
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+
+(define (cond-predicate clause) (car clause))
+
+(define (cond-actions clause) (cdr clause))
+
+(define (cond->if exp)
+  (expand-clauses (cond-clauses exp)))
+
+(define (expand-clauses clauses)
+  (if (null? clauses)
+      'false                          ; else節なし
+      (let ((first (car clauses))
+            (rest (cdr clauses)))
+        (if (cond-else-clause? first)
+            (if (null? rest)
+                (sequence->exp (cond-actions first))
+                (error "ELSE clause isn't last -- COND->IF"
+                       clauses))
+            (make-if (cond-predicate first)
+                     (sequence->exp (cond-actions first))
+                     (expand-clauses rest))))))
+
+
+; https://sicp.iijlab.net/fulltext/x413.html
+
+; 述語のテスト
+(define (true? x)
+  (not (eq? x false)))
+
+(define (false? x)
+  (eq? x false))
+
+; 手続きの表現
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+
+(define (procedure-parameters p) (cadr p))
+
+(define (procedure-body p) (caddr p))
+
+(define (procedure-environment p) (cadddr p))
+
+; 環境に対する操作
+(define (enclosing-environment env) (cdr env))
+
+(define (first-frame env) (car env))
+
+(define the-empty-environment '())
+
+(define (make-frame variables values)
+  (cons variables values))
+
+(define (frame-variables frame) (car frame))
+
+(define (frame-values frame) (cdr frame))
+
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons var (car frame)))
+  (set-cdr! frame (cons val (cdr frame))))
+
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (car vals))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable -- SET!" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (add-binding-to-frame! var val frame))
+            ((eq? var (car vars))
+             (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (scan (frame-variables frame)
+          (frame-values frame))))
+
+
+; https://sicp.iijlab.net/fulltext/x414.html
+(define (setup-environment)
+  (let ((initial-env
+         (extend-environment (primitive-procedure-names)
+                             (primitive-procedure-objects)
+                             the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+
+(define (primitive-implementation proc) (cadr proc))
+
+(define primitive-procedures
+  (list (list 'car car)
+        (list 'cdr cdr)
+        (list 'cons cons)
+        (list 'null? null?)
+        (list '+ +)
+        (list '- -)
+        (list '* *)
+        (list '= =)
+        (list '> >)
+        (list '<= <=)
+        (list 'eq? eq?)
+        (list 'not not)
+        (list 'remainder remainder)
+        (list 'list list)
+        (list 'member member)
+        (list 'abs abs)
+        ;⟨基本手続きが続く⟩
+        ))
+
+(define (primitive-procedure-names)
+  (map car
+       primitive-procedures))
+
+(define (primitive-procedure-objects)
+  (map (lambda (proc) (list 'primitive (cadr proc)))
+       primitive-procedures))
+
+; setup-environment 内部で primitive-procedure-names と primitive-procedure-objects を利用するため実行タイミングをここにした。
+(define the-global-environment (setup-environment))
+
+(define (apply-primitive-procedure proc args)
+  (apply-in-underlying-scheme
+   (primitive-implementation proc) args))
+
+(define apply-in-underlying-scheme apply)
+
+; https://wizardbook.wordpress.com/2010/12/26/exercise-4-6/
+(define (let-initials exp) 
+  (map cadr (cadr exp)))
+(define (let-parameters exp)
+  (map car (cadr exp)))
+(define (let-body exp)
+  (cddr exp))
+   
+; a let is syntactic sugar for
+;   ((lambda (params) (body)) values)
+(define (let->combination exp)
+  (cons (make-lambda (let-parameters exp) 
+                     (let-body exp))
+        (let-initials exp)))
+ 
+(define (let? exp) (tagged-list? exp 'let))
+
+; https://sicp.iijlab.net/fulltext/x433.html
+(define (amb? exp) (tagged-list? exp 'amb))
+
+(define (amb-choices exp) (cdr exp))
+
+(define (ambeval exp env succeed fail)
+  ((analyze exp) env succeed fail))
+
+(define (analyze-self-evaluating exp)
+  (lambda (env succeed fail)
+    (succeed exp fail)))
+
+(define (analyze-quoted exp)
+  (let ((qval (text-of-quotation exp)))
+    (lambda (env succeed fail)
+      (succeed qval fail))))
+
+(define (analyze-variable exp)
+  (lambda (env succeed fail)
+    (succeed (lookup-variable-value exp env)
+             fail)))
+
+(define (analyze-lambda exp)
+  (let ((vars (lambda-parameters exp))
+        (bproc (analyze-sequence (lambda-body exp))))
+    (lambda (env succeed fail)
+      (succeed (make-procedure vars bproc env)
+               fail))))
+
+(define (analyze-if exp)
+  (let ((pproc (analyze (if-predicate exp)))
+        (cproc (analyze (if-consequent exp)))
+        (aproc (analyze (if-alternative exp))))
+    (lambda (env succeed fail)
+      (pproc env
+             ;; pred-valueを得るための
+             ;; 述語の評価の成功継続
+             (lambda (pred-value fail2)
+               (if (true? pred-value)
+                   (cproc env succeed fail2)
+                   (aproc env succeed fail2)))
+             ;; 述語の評価の失敗継続
+             fail))))
+
+(define (analyze-sequence exps)
+  (define (sequentially a b)
+    (lambda (env succeed fail)
+      (a env
+         ;; aを呼び出す時の成功継続
+         (lambda (a-value fail2)
+           (b env succeed fail2))
+         ;; aを呼び出す時の失敗継続
+         fail)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+        first-proc
+        (loop (sequentially first-proc (car rest-procs))
+              (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+        (error "Empty sequence -- ANALYZE"))
+    (loop (car procs) (cdr procs))))
+
+(define (analyze-definition exp)
+  (let ((var (definition-variable exp))
+        (vproc (analyze (definition-value exp))))
+    (lambda (env succeed fail)
+      (vproc env                        
+             (lambda (val fail2)
+               (define-variable! var val env)
+               (succeed 'ok fail2))
+             fail))))
+
+(define (analyze-assignment exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail2)        ; *1*
+               (let ((old-value
+                      (lookup-variable-value var env))) 
+                 (set-variable-value! var val env)
+                 (succeed 'ok
+                          (lambda ()    ; *2*
+                            (set-variable-value! var
+                                                 old-value
+                                                 env)
+                            (fail2)))))
+             fail))))
+
+(define (analyze-application exp)
+  (let ((pproc (analyze (operator exp)))
+        (aprocs (map analyze (operands exp))))
+    (lambda (env succeed fail)
+      (pproc env
+             (lambda (proc fail2)
+               (get-args aprocs
+                         env
+                         (lambda (args fail3)
+                           (execute-application
+                            proc args succeed fail3))
+                         fail2))
+             fail))))
+
+(define (get-args aprocs env succeed fail)
+  (if (null? aprocs)
+      (succeed '() fail)
+      ((car aprocs) env
+                    ;; このaprocの成功継続
+                    (lambda (arg fail2)
+                      (get-args (cdr aprocs)
+                                env
+                                ;; get-argsの再帰呼出しの
+                                ;; 成功継続
+                                (lambda (args fail3)
+                                  (succeed (cons arg args)
+                                           fail3))
+                                fail2))
+                    fail)))
+
+(define (execute-application proc args succeed fail)
+  (cond ((primitive-procedure? proc)
+         (succeed (apply-primitive-procedure proc args)
+                  fail))
+        ((compound-procedure? proc)
+         ((procedure-body proc)
+          (extend-environment (procedure-parameters proc)
+                              args
+                              (procedure-environment proc))
+          succeed
+          fail))
+        (else
+         (error
+          "Unknown procedure type -- EXECUTE-APPLICATION"
+          proc))))
+
+(define (analyze-amb exp)
+  (let ((cprocs (map analyze (amb-choices exp))))
+    (lambda (env succeed fail)
+      (define (try-next choices)
+        (if (null? choices)
+            (fail)
+            ((car choices) env
+                           succeed
+                           (lambda ()
+                             (try-next (cdr choices))))))
+      (try-next cprocs))))
+
+; 既存と同じように動作するように eval を作成した。また env は the-global-environment 固定とした。
+(define (eval exp)
+  (ambeval exp the-global-environment
+           (lambda (value next-alternative)
+             (set! global-next-alternative next-alternative)
+             value)
+           (lambda () 'failed)))
+
+; try-again を実装するため global-next-alternative 変数を用意した。
+(define global-next-alternative nil)
+
+(define (try-again)
+  (global-next-alternative))
+
+; 追加実装箇所
+; https://wizardbook.wordpress.com/2011/01/12/exercise-4-42/
+; and と or を利用していたため実装した。
+(define (analyze exp)
+  (cond ((self-evaluating? exp) 
+         (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((let? exp) (analyze (let->combination exp)))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((and? exp) (analyze-and (and-actions exp)))
+        ((or? exp) (analyze-or (or-actions exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((amb? exp) (analyze-amb exp))
+        ((application? exp) (analyze-application exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
+
+; and
+(define (and? exp) (tagged-list? exp 'and))
+
+(define (and-actions exp) (cdr exp))
+
+(define (analyze-and exps)
+  (define (sequentially a b)
+    (lambda (env succeed fail)
+      (a env
+         ;; aを呼び出す時の成功継続
+         (lambda (a-value fail2)
+           (if (true? a-value) 
+               (b env succeed fail2)
+               (succeed false fail2)))
+         ;; aを呼び出す時の失敗継続
+         fail)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+        first-proc
+        (loop (sequentially first-proc (car rest-procs))
+              (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+        (error "Empty and -- ANALYZE"))
+    (loop (car procs) (cdr procs))))
+
+; or
+(define (or? exp) (tagged-list? exp 'or))
+
+(define (or-actions exp) (cdr exp))
+
+(define (analyze-or exps)
+  (define (sequentially a b)
+    (lambda (env succeed fail)
+      (a env
+         ;; aを呼び出す時の成功継続
+         (lambda (a-value fail2)
+           (if (true? a-value) 
+               (succeed true fail2)
+               (b env succeed fail2)))
+         ;; aを呼び出す時の失敗継続
+         fail)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+        first-proc
+        (loop (sequentially first-proc (car rest-procs))
+              (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+        (error "Empty or -- ANALYZE"))
+    (loop (car procs) (cdr procs))))
+
+; 以下、動作確認
+; 回帰テスト
+(#%require (only rackunit check-equal?))
+(check-equal? (eval '(define x 3)) 'ok)
+(check-equal? (eval '(+ x 2)) 5)
+(check-equal? (eval '(quote x)) 'x)
+(check-equal? (eval '(set! x 5)) 'ok)
+(check-equal? (eval '(+ x 2)) 7)
+(check-equal? (eval '(if true x)) 5)
+(check-equal? (eval '(begin (+ x 2)(- x 2))) 3)
+(check-equal? (eval '((lambda (y) (+ y y)) x)) 10)
+(check-equal? (eval '(cond ((eq? x 5) x)(else false))) 5)
+(check-equal? (eval '(cond ((eq? x 4) x)(else false))) false)
+(check-equal? (eval '(let ((y x)) (+ y y))) 10)
+(check-equal? (eval '(and true false)) false)
+(check-equal? (eval '(and true true)) true)
+(check-equal? (eval '(or false false)) false)
+
+; 追加実装箇所
+(eval
+ '(define (require p)
+    (if (not p) (amb)))
+ )
+
+(eval
+ '(define (distinct? items)
+    (cond ((null? items) true)
+          ((null? (cdr items)) true)
+          ((member (car items) (cdr items)) false)
+          (else (distinct? (cdr items)))))
+ )
+
+; https://wizardbook.wordpress.com/2011/01/12/exercise-4-42/
+(eval
+ '(define (xor a b)
+    (or (and a (not b))
+        (and (not a) b)))
+ )
+
+(eval
+ '(define (betty-statement betty kitty)
+    (xor (= kitty 2) (= betty 3)))
+ )
+
+(eval
+ '(define (ethel-statement ethel joan)
+    (xor (= ethel 1) (= joan 2)))
+ )
+
+(eval
+ '(define (joan-statement joan ethel)
+    (xor (= joan 3) (= ethel 5)))
+ )
+
+(eval
+ '(define (kitty-statement kitty mary)
+    (xor (= kitty 2) (= mary 4)))
+ )
+
+(eval
+ '(define (mary-statement mary betty)
+    (xor (= mary 4) (= betty 1)))
+ )
+
+(eval
+ '(define (amb-liars)
+    (let ((betty (amb 1 2 3 4 5))
+          (kitty (amb 1 2 3 4 5)))
+      (require (betty-statement betty kitty))
+      (let ((mary (amb 1 2 3 4 5)))
+        (require (mary-statement mary betty))
+        (require (kitty-statement kitty mary))
+        (let ((joan (amb 1 2 3 4 5))
+              (ethel (amb 1 2 3 4 5)))
+          (require (ethel-statement ethel joan))
+          (require (joan-statement joan ethel))
+          (require (distinct? (list betty ethel joan kitty mary)))
+          (list
+           (list 'betty betty)
+           (list 'ethel ethel)
+           (list 'joan joan)
+           (list 'kitty kitty)
+           (list 'mary mary))))))
+ )
+
+(eval '(amb-liars))
+
+(display '"all ok")
