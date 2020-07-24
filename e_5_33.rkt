29ebbca9b8482f5a2cf386f67c4ebe4b27ac64d3
n3104 <nt3104@gmail.com>
2020-07-24 20:59:23 +0900

    問題 5.33 命令列を出力するために display-compile-result 関数を追加した。

--- e_5_33.rkt
+++ e_5_33.rkt
@@ -5,22 +5,29 @@
 (include "./ch5-compiler.rkt")
 
 
+(define (display-compile-result result)
+  (for-each (lambda (x) (display x) (newline))
+            (cons (car result) (cons (cadr result) (caddr result)))))
+
 (display 'factorial)
 (newline)
-(compile
- '(define (factorial n)
-    (if (= n 1)
-        1
-        (* (factorial (- n 1)) n)))
- 'val
- 'next)
+(display-compile-result
+ (compile
+  '(define (factorial n)
+     (if (= n 1)
+         1
+         (* (factorial (- n 1)) n)))
+  'val
+  'next))
 
+(newline)
 (display 'factorial-alt)
 (newline)
-(compile
- '(define (factorial-alt n)
-    (if (= n 1)
-        1
-        (* n (factorial-alt (- n 1)))))
- 'val
- 'next)
+(display-compile-result
+ (compile
+  '(define (factorial-alt n)
+     (if (= n 1)
+         1
+         (* n (factorial-alt (- n 1)))))
+  'val
+  'next))
