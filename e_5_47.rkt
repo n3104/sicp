c1cbd8def9c4d00066c6f28baa32dd14c2e75317
n3104 <nt3104@gmail.com>
2020-09-13 21:02:20 +0900

    問題 5.47 実装した。

--- e_5_47.rkt
+++ e_5_47.rkt
@@ -12,6 +12,7 @@
 
 (define (compile-procedure-call-with-interpreted-procedures target linkage)
   (let ((primitive-branch (make-label 'primitive-branch))
+        (compound-branch (make-label 'compound-branch))
         (compiled-branch (make-label 'compiled-branch))
         (after-call (make-label 'after-call)))
     (let ((compiled-linkage
@@ -20,19 +21,26 @@
        (make-instruction-sequence '(proc) '()
         `((test (op primitive-procedure?) (reg proc))
           (branch (label ,primitive-branch))))
+       (make-instruction-sequence '(proc) '()
+        `((test (op compound-procedure?) (reg proc))
+          (branch (label ,compound-branch))))
        (parallel-instruction-sequences
         (append-instruction-sequences
          compiled-branch
          (compile-proc-appl target compiled-linkage))
-        (append-instruction-sequences
-         primitive-branch
-         (end-with-linkage linkage
-          (make-instruction-sequence '(proc argl)
-                                     (list target)
-           `((assign ,target
-                     (op apply-primitive-procedure)
-                     (reg proc)
-                     (reg argl)))))))
+        (parallel-instruction-sequences
+         (append-instruction-sequences
+          compound-branch
+          (compound-proc-appl target compiled-linkage))
+         (append-instruction-sequences
+          primitive-branch
+          (end-with-linkage linkage
+           (make-instruction-sequence '(proc argl)
+                                      (list target)
+            `((assign ,target
+                      (op apply-primitive-procedure)
+                      (reg proc)
+                      (reg argl))))))))
        after-call))))
 
 (set! compile-procedure-call compile-procedure-call-with-interpreted-procedures)
@@ -41,25 +49,22 @@
   (cond ((and (eq? target 'val) (not (eq? linkage 'return)))
          (make-instruction-sequence '(proc) all-regs
            `((assign continue (label ,linkage))
-             (assign val (op compiled-procedure-entry)
-                         (reg proc))
-             (goto (reg val)))))
+             (save continue)
+             (goto (reg compapp)))))
         ((and (not (eq? target 'val))
               (not (eq? linkage 'return)))
          (let ((proc-return (make-label 'proc-return)))
            (make-instruction-sequence '(proc) all-regs
             `((assign continue (label ,proc-return))
-              (assign val (op compiled-procedure-entry)
-                          (reg proc))
-              (goto (reg val))
+              (save continue)
+              (goto (reg compapp))
               ,proc-return
               (assign ,target (reg val))
               (goto (label ,linkage))))))
         ((and (eq? target 'val) (eq? linkage 'return))
          (make-instruction-sequence '(proc continue) all-regs
-          '((assign val (op compiled-procedure-entry)
-                        (reg proc))
-            (goto (reg val)))))
+          `((save continue)
+            (goto (reg compapp)))))
         ((and (not (eq? target 'val)) (eq? linkage 'return))
          (error "return linkage, target not val -- COMPILE"
                 target))))
@@ -68,7 +73,7 @@
  '(define (f)
     (g)))
 
-; 以下動作確認手順とその結果。最初はうまく動作しない。
+; 以下動作確認手順とその結果
 ;(total-pushes = 0 maximum-depth = 0)
 ;;;; EC-Eval value:
 ;ok
@@ -82,4 +87,7 @@
 ;
 ;;;; EC-Eval input:
 ;(f)
-;'done
+;
+;(total-pushes = 4 maximum-depth = 3)
+;;;; EC-Eval value:
+;3
